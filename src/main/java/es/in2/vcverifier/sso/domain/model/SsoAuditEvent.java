cf08f59de5b11500321272af3992b5e1b64e19aa
jesusdaniel.gonzalez <jesusdaniel.gonzalez@altia.es>
2026-06-08 12:39:13 +0200

    EUDISTACK-543 - US2-Sesión SSO establecida tras presentación OID4VP exitosa

--- src/main/java/es/in2/vcverifier/sso/domain/model/SsoAuditEvent.java
+++ src/main/java/es/in2/vcverifier/sso/domain/model/SsoAuditEvent.java
@@ -1,8 +1,11 @@
 package es.in2.vcverifier.sso.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.time.Instant;
-import java.util.Objects;
 
+@AllArgsConstructor
+@Data
 public class SsoAuditEvent {
 
 
@@ -22,51 +25,8 @@ public class SsoAuditEvent {
     private final String correlationId;
     private final Instant occurredAt;
 
-    public SsoAuditEvent(
-            EventType eventType,
-            String tenant,
-            String clientId,
-            String holderHash,
-            String outcome,
-            String correlationId,
-            Instant occurredAt
-    ) {
-        this.eventType = Objects.requireNonNull(eventType);
-        this.tenant = Objects.requireNonNull(tenant);
-        this.clientId = clientId;
-        this.holderHash = holderHash;
-        this.outcome = outcome;
-        this.correlationId = correlationId;
-        this.occurredAt = occurredAt != null ? occurredAt : Instant.now();
-    }
 
-    public EventType getEventType() {
-        return eventType;
-    }
 
-    public String getTenant() {
-        return tenant;
-    }
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getHolderHash() {
-        return holderHash;
-    }
-
-    public String getOutcome() {
-        return outcome;
-    }
-
-    public String getCorrelationId() {
-        return correlationId;
-    }
-
-    public Instant getOccurredAt() {
-        return occurredAt;
-    }
 
 
 
