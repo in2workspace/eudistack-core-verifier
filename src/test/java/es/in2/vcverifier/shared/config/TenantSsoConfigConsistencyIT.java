a10c27ec1a7efa35a0b20cef3f2ebb81bfb03793
jesusdaniel.gonzalez <jesusdaniel.gonzalez@altia.es>
2026-06-09 08:19:20 +0200

    EUDISTACK-543-US-02: Sesión SSO establecida tras presentación OID4VP exitosa

--- src/test/java/es/in2/vcverifier/shared/config/TenantSsoConfigConsistencyIT.java
+++ src/test/java/es/in2/vcverifier/shared/config/TenantSsoConfigConsistencyIT.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
