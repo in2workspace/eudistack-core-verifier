952a2e59d499dcc53cddbc8691a51a5b347d7679
Albert Rodríguez <albert.rodriguez@altia.es>
2026-07-27 14:11:15 +0200

    fix(metrics): refactor application name handling and add SpringApplicationProperties configuration

--- src/main/java/es/in2/vcverifier/shared/config/MicrometerMetricsConfig.java
+++ src/main/java/es/in2/vcverifier/shared/config/MicrometerMetricsConfig.java
@@ -1,19 +1,22 @@
 package es.in2.vcverifier.shared.config;
 
+import es.in2.vcverifier.shared.config.properties.SpringApplicationProperties;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class MicrometerMetricsConfig {
 
+    private final SpringApplicationProperties springApplicationProperties;
+
     @Bean
-    public MeterRegistryCustomizer<MeterRegistry> commonTagsCustomizer(
-            @Value("${spring.application.name}") String appName) {
+    public MeterRegistryCustomizer<MeterRegistry> commonTagsCustomizer() {
         return registry -> registry.config().commonTags(
-                "application", appName,
+                "application", springApplicationProperties.name(),
                 "component", "verifier-backend"
         );
     }
