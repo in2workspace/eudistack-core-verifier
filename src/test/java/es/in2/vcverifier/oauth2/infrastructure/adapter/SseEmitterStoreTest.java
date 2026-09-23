97e8c5090063ac3a6fb1742c578d433469a34693
Auribel Alvarado <auribel.alvarado@in2.es>
2026-09-23 09:44:00 +0200

    Simplify `mockConstruction` usage in `SseEmitterStoreTest` with `var`.

--- src/test/java/es/in2/vcverifier/oauth2/infrastructure/adapter/SseEmitterStoreTest.java
+++ src/test/java/es/in2/vcverifier/oauth2/infrastructure/adapter/SseEmitterStoreTest.java
@@ -129,7 +129,7 @@ class SseEmitterStoreTest {
                 }),
                 any(Instant.class), any(Duration.class));
 
-        try (MockedConstruction<SseEmitter> _ = mockConstruction(SseEmitter.class)) {
+        try (var _ = mockConstruction(SseEmitter.class)) {
             SseEmitter emitter = store.create("state-1", 60000L);
 
             capturedHeartbeat.get().run();
@@ -148,7 +148,7 @@ class SseEmitterStoreTest {
                 }),
                 any(Instant.class), any(Duration.class));
 
-        try (MockedConstruction<SseEmitter> _ = mockConstruction(SseEmitter.class,
+        try (var _ = mockConstruction(SseEmitter.class,
                 (mock, context) -> doThrow(new IOException("client disconnected"))
                         .when(mock).send(any(SseEmitter.SseEventBuilder.class)))) {
             store.create("state-1", 60000L);
